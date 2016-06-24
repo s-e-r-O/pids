2baeea6b5cf70bf59dd7910246b29b35716572ad
Isabella Defilippis Villarroel <isastedef@yahoo.com>
2016-06-24 08:16:02 -0400

    Puzzle jarrones

--- Assets/scripts/Cursor.cs
+++ Assets/scripts/Cursor.cs
@@ -16,10 +16,6 @@ public class Cursor : MonoBehaviour {
 	}
 
 	public GameObject getPointedObject(){
-<<<<<<< HEAD
-=======
-
->>>>>>> 44f5ac2569161ef5fbf039a805c86aebe3c49045
 		int x = Screen.width / 2;
 		int y = Screen.height / 2;
 		Ray ray = this.mainCamera.GetComponent<Camera>().ScreenPointToRay (new Vector3 (x, y));
---
+++ Assets/scripts/TextureChanger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureChanger : MonoBehaviour {
+
+    public Texture text1;
+    public Texture text2;
+    public Texture text3;
+    public Texture text4;
+    public int number;
+
+	void Start () {
+        Texture text = random_Text();
+        GetComponent<Renderer>().material.mainTexture = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private Texture random_Text()
+    {
+        number = Random.Range(0, 4);
+        Texture text = new Texture();
+        switch (number)
+        {
+            case 0:
+                text = text1;
+                break;
+            case 1:
+                text = text2;
+                break;
+            case 2:
+                text = text3;
+                break;
+            case 3:
+                text = text4;
+                break;
+        }
+        return text;
+    }
+}
